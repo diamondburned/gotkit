02f1d83e4d66f055def3cc39dc8c1c1c634f6e65
diamondburned <diamond@arikawa-hi.me>
2022-04-28 22:41:29 -0700

    Use gotk4-nix

--- shell.nix
+++ shell.nix
@@ -1,1 +1,15 @@
-.nix/shell.nix
+{ pkgs ? import <nixpkgs> {} }:
+
+let gotk4-nix = pkgs.fetchFromGitHub {
+		owner = "diamondburned";
+		repo  = "gotk4-nix";
+		rev   = "b5bb50b31ffd7202decfdb8e84a35cbe88d42c64";
+		hash  = "sha256:18wxf24shsra5y5hdbxqcwaf3abhvx1xla8k0vnnkrwz0r9n4iqq";
+	};
+
+in import "${gotk4-nix}/shell.nix" {
+	base = {
+		pname = "gotkit";
+		version = "dev";
+	};
+}
