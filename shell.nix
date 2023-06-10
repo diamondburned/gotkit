bf92208f60690c3638c5679802910f1ec37ca1e4
diamondburned <diamond@arikawa-hi.me>
2023-06-10 00:28:17 -0700

    Bump gotk4-nix

--- shell.nix
+++ shell.nix
@@ -2,9 +2,9 @@
 
 let gotk4-nix = pkgs.fetchFromGitHub {
 		owner = "diamondburned";
-		repo  = "gotk4-nix";
-		rev   = "b5bb50b31ffd7202decfdb8e84a35cbe88d42c64";
-		hash  = "sha256:18wxf24shsra5y5hdbxqcwaf3abhvx1xla8k0vnnkrwz0r9n4iqq";
+		repo = "gotk4-nix";
+		rev = "b186ac5419c22f4b75b1bdc35ef4fd9349c6b65e";
+		sha256 = "1pfx0p4w56d7pa1l9ipqfq52znfl7slc2wbjfqddq1jv1fp9z43q";
 	};
 
 in import "${gotk4-nix}/shell.nix" {
