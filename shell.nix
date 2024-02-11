15e0c13bd405bcd13a2582fd4610bf8eecd52168
diamondburned <diamond@arikawa-hi.me>
2024-02-10 23:37:31 -0800

    Update gotk4-nix

--- shell.nix
+++ shell.nix
@@ -4,8 +4,8 @@ let
 	gotk4-nix = pkgs.fetchFromGitHub {
 		owner  = "diamondburned";
 		repo   = "gotk4-nix";
-		rev    = "ad91dabf706946c4380d0a105f0937e4e8ffd75f";
-		sha256 = "0rkw9k98qy7ifwypkh2fqhdn7y2qphy2f8xjisj0cyp5pjja62im";
+		rev    = "4eab6a0";
+		sha256 = "sha256-WsJ2Cf1hvKT3BUYYVxQ5rNMYi6z7NWccbSsw39lgqO8=";
 	};
 in
 
@@ -19,10 +19,4 @@ import "${gotk4-nix}/shell.nix" {
 		# staticcheck takes forever to build gotk4 twice. I'm good.
 		(writeShellScriptBin "staticcheck" "")
 	];
-
-	shellHook = ''
-		# Generate compile_flags.txt with the needed pkg-config flags.
-		readarray -d' ' cflags < <(pkg-config --cflags --libs gtk4)
-		printf "%s\n" "''${cflags[@]}" > compile_flags.txt
-	'';
 }
