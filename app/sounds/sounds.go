6b38d79e7bb10b24d9f29c37e1b21c06620d6a26
diamondburned <diamond@libdb.so>
2024-08-03 22:00:13 +0700

    Refactor how app/sounds plays sounds

--- app/sounds/sounds.go
+++ app/sounds/sounds.go
@@ -2,8 +2,9 @@ package sounds
 
 import (
 	"embed"
-	"io"
-	"log"
+	"fmt"
+	"io/fs"
+	"log/slog"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -14,11 +15,14 @@ import (
 	"github.com/diamondburned/gotk4/pkg/gdk/v4"
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 	"github.com/diamondburned/gotkit/app"
-	"github.com/pkg/errors"
+	"github.com/diamondburned/gotkit/utils/osutil"
 )
 
 //go:embed *.opus
-var sounds embed.FS
+var embeddedSoundsFS embed.FS
+
+// SoundsFS is a list of filesystems to search for sounds.
+var SoundsFS = []fs.FS{embeddedSoundsFS}
 
 // Sound IDs.
 const (
@@ -27,9 +31,46 @@ const (
 )
 
 var (
-	fileExistsCache  sync.Map
-	soundsLastPlayed sync.Map
+	loadedSounds   = map[string]loadedSound{}
+	loadedSoundsMu sync.RWMutex
 )
+
+type loadedSound struct {
+	playing bool
+	file    *gtk.MediaFile
+}
+
+func getLoadedSound(id string) (loadedSound, bool) {
+	loadedSoundsMu.RLock()
+	defer loadedSoundsMu.RUnlock()
+
+	sound, ok := loadedSounds[id]
+	return sound, ok
+}
+
+func setLoadedSound(id string, sound loadedSound) {
+	loadedSoundsMu.Lock()
+	defer loadedSoundsMu.Unlock()
+
+	loadedSounds[id] = sound
+}
+
+func setLoadedSoundPlaying(id string, playing bool) {
+	loadedSoundsMu.Lock()
+	defer loadedSoundsMu.Unlock()
+
+	if sound, ok := loadedSounds[id]; ok {
+		sound.playing = playing
+		loadedSounds[id] = sound
+	}
+}
+
+func unloadSound(id string) {
+	loadedSoundsMu.Lock()
+	defer loadedSoundsMu.Unlock()
+
+	delete(loadedSounds, id)
+}
 
 const soundDebounce = 200 * time.Millisecond
 
@@ -44,105 +85,179 @@ func Play(app *app.Application, id string) {
 }
 
 func play(app *app.Application, id string) {
-	now := time.Now()
-
-	if t, ok := soundsLastPlayed.Load(id); ok {
-		t := t.(time.Time)
-		if now.Sub(t) < soundDebounce {
+	sound, ok := getLoadedSound(id)
+	if !ok {
+		// If we can play with Canberra, we don't need to load the sound.
+		// Mark the sound as loaded to prevent future loading.
+		if playWithCanberra(id) {
+			setLoadedSound(id, loadedSound{})
 			return
 		}
-	}
 
-	soundsLastPlayed.Store(id, now)
+		soundFilename := id
+		if filepath.Ext(soundFilename) == "" {
+			soundFilename += ".opus"
+		}
 
-	canberra := exec.Command("canberra-gtk-play", "--id", id)
-	if err := canberra.Run(); err == nil {
-		return
-	} else {
-		log.Println("canberra error:", err)
-	}
+		soundFilepath := app.CachePath("sounds", soundFilename)
 
-	name := id
-	if filepath.Ext(name) == "" {
-		name += ".opus"
-	}
+		if _, err := os.Stat(soundFilepath); err != nil {
+			if !os.IsNotExist(err) {
+				slog.Error(
+					"cannot stat sound file, playing fallback beep",
+					"module", "sounds",
+					"err", err,
+					"id", id,
+					"path", soundFilepath)
+				beep()
+				return
+			}
 
-	dst := app.CachePath("sounds", name)
-
-	var fileExists bool
-	if b, ok := fileExistsCache.Load(dst); ok && b.(bool) {
-		fileExists = true
-	} else {
-		_, err := os.Stat(dst)
-		if err != nil {
-			if err := copyToFS(dst, name); err != nil {
-				log.Printf("cannot copy sound %q: %v", id, err)
-				glib.IdleAdd(beep)
+			if err := copyToFS(soundFilepath, soundFilename); err != nil {
+				slog.Error(
+					"cannot copy sound file to disk, playing fallback beep",
+					"module", "sounds",
+					"err", err,
+					"id", id,
+					"path", soundFilepath)
+				beep()
 				return
 			}
 		}
-		fileExists = true
-		fileExistsCache.Store(id, fileExists)
-	}
 
-	glib.IdleAdd(func() {
-		media := gtk.NewMediaFileForFilename(dst)
+		glib.IdleAdd(func() {
+			var soundFile *gtk.MediaFile
 
-		mediaWeak := glib.NewWeakRef(media)
-		media.NotifyProperty("error", func() {
-			media := mediaWeak.Get()
-			fileExistsCache.Delete(id)
-			playEmbedError(id, media.Error())
+			if sound, ok := getLoadedSound(id); ok {
+				soundFile = sound.file
+				setLoadedSoundPlaying(id, true)
+			} else {
+				slog.Debug(
+					"creating new media file for sound",
+					"module", "sounds",
+					"id", id,
+					"path", soundFilepath)
+
+				soundFile = gtk.NewMediaFileForFilename(soundFilepath)
+				soundFile.NotifyProperty("error", func() {
+					slog.Error(
+						"could not load sound file, playing fallback beep",
+						"module", "sounds",
+						"err", soundFile.Error(),
+						"id", id,
+						"path", soundFilepath)
+					beep()
+					unloadSound(id)
+				})
+				soundFile.NotifyProperty("playing", func() {
+					if soundFile.Playing() {
+						slog.Debug(
+							"playing sound with loaded media file",
+							"module", "sounds",
+							"id", id,
+							"path", soundFilepath)
+					} else {
+						slog.Debug(
+							"sound file stopped playing",
+							"module", "sounds",
+							"id", id,
+							"path", soundFilepath)
+						setLoadedSoundPlaying(id, false)
+					}
+				})
+
+				setLoadedSound(id, loadedSound{
+					playing: true,
+					file:    soundFile,
+				})
+			}
+
+			soundFile.Play()
 		})
 
-		media.Play()
-	})
+		return
+	}
+
+	if sound.playing {
+		slog.Debug(
+			"not playing sound, already playing",
+			"module", "sounds",
+			"id", id)
+		return
+	}
+
+	slog.Debug(
+		"sound loaded from cache, playing",
+		"module", "sounds",
+		"id", id,
+		"is_media", sound.file != nil)
+
+	if sound.file != nil {
+		glib.IdleAdd(func() {
+			setLoadedSoundPlaying(id, true)
+			sound.file.Play()
+		})
+		return
+	}
+
+	if playWithCanberra(id) {
+		return
+	}
+
+	// If Canberra fails after a successful play, we'll wipe the cache
+	// and play the sound again.
+	unloadSound(id)
+	play(app, id)
 }
 
-func playEmbedError(name string, err error) {
-	log.Printf("error playing embedded %s.opus: %v", name, err)
-	beep()
+var enableCanberra = true
+
+func playWithCanberra(id string) bool {
+	if !enableCanberra {
+		return false
+	}
+
+	slog.Debug(
+		"playing sound with canberra",
+		"module", "sounds",
+		"id", id)
+
+	cmd := exec.Command("canberra-gtk-play", "--id", id)
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Run(); err != nil {
+		slog.Error(
+			"failed to play sound with canberra",
+			"module", "sounds",
+			"id", id,
+			"err", err)
+		return false
+	}
+
+	return true
 }
 
 func beep() {
-	log.Println("using beep() instead")
-	disp := gdk.DisplayGetDefault()
-	disp.Beep()
+	glib.IdleAdd(func() {
+		disp := gdk.DisplayGetDefault()
+		disp.Beep()
+	})
 }
 
 func copyToFS(dst string, name string) error {
-	src, err := sounds.Open(name)
-	if err != nil {
-		return err
+	var soundData []byte
+
+	for _, sounds := range SoundsFS {
+		d, err := fs.ReadFile(sounds, name)
+		if err == nil {
+			soundData = d
+			break
+		}
 	}
 
-	defer src.Close()
-
-	dir := filepath.Dir(dst)
-
-	if err := os.MkdirAll(dir, os.ModePerm); err != nil {
-		return errors.Wrap(err, "cannot mkdir sounds/")
+	if soundData == nil {
+		return fmt.Errorf("embedded sound not found: %s", name)
 	}
 
-	f, err := os.CreateTemp(dir, ".tmp.*")
-	if err != nil {
-		return errors.Wrap(err, "cannot mktemp in cache dir")
-	}
-
-	defer os.Remove(f.Name())
-	defer f.Close()
-
-	if _, err := io.Copy(f, src); err != nil {
-		return errors.Wrap(err, "cannot write audio to disk")
-	}
-
-	if err := f.Close(); err != nil {
-		return errors.Wrap(err, "cannot close written audio")
-	}
-
-	if err := os.Rename(f.Name(), dst); err != nil {
-		return errors.Wrap(err, "cannot commit written audio")
-	}
-
-	return nil
+	return osutil.WriteFile(dst, soundData)
 }
--- app/sounds/sounds_test.go
+++ app/sounds/sounds_test.go
@@ -1,40 +1,71 @@
-package sounds_test
+package sounds
 
 import (
 	"context"
+	"log/slog"
+	"testing"
 
 	"github.com/diamondburned/gotk4/pkg/glib/v2"
 	"github.com/diamondburned/gotkit/app"
-	"github.com/diamondburned/gotkit/app/sounds"
+	"github.com/neilotoole/slogt"
 )
 
-func ExamplePlay() {
+func TestPlay(t *testing.T) {
+	injectLogger(t)
+
+	enableCanberra = false
+	t.Cleanup(func() { enableCanberra = true })
+
 	app := app.New(context.Background(), "com.example.app", "app")
 	app.ConnectActivate(func() {
-		// Plays are automatically debounced.
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-		sounds.Play(app, sounds.Message)
-
 		app.Hold()
-		glib.TimeoutSecondsAdd(1, app.Release)
+
+		spamSounds := func() {
+			// Plays are automatically debounced.
+			Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+			// Play(app, Message)
+		}
+
+		var i int
+		glib.TimeoutSecondsAdd(1, func() bool {
+			spamSounds()
+			i++
+
+			if i >= 10 {
+				app.Release()
+				return false
+			}
+
+			return true
+		})
 	})
 
 	app.Run(nil)
 	// Output:
+}
+
+func injectLogger(t *testing.T) {
+	t.Helper()
+
+	oldLogger := slog.Default()
+	newLogger := slogt.New(t)
+
+	slog.SetDefault(newLogger)
+	t.Cleanup(func() { slog.SetDefault(oldLogger) })
 }
